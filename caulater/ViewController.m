77525049195c6c9bf40f390c13ae64aac5823f30
xidiyixiwocao <343382542@qq.com>
2019-06-28 16:37:55 +0800

    33qq

--- caulater/ViewController.m
+++ caulater/ViewController.m
@@ -13,7 +13,7 @@
 @property(nonatomic,strong) CBCentralManager* myManager;
 @property(nonatomic,strong) CBPeripheral* myPeripheral;
 @property(nonatomic,strong) NSMutableArray<NSString*>* nameArray;//testGit
-@end//tessAgain245
+@end//tessAgain2456
 
 @implementation ViewController
 
